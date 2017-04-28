e410e9a659ccd385fa68f3e218e2b8ae1c07172e
marc <contact@marccollin.com>
2017-04-28 10:22:25 +0200

    init

---
+++ config/settings.old.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'psp16113');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'Zy5EDXuGH1JsjeeMz5LIKUfJ21Z2renBbrM0iMef9Ca1vee3KsOYwTzY');
+define('_COOKIE_IV_', 'Lq8JJtUc');
+define('_PS_CREATION_DATE_', '2017-04-28');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.13');
+define('_RIJNDAEL_KEY_', 'feNu5VVYOVm8eBf2tbqVRLNwxPwGaRrV');
+define('_RIJNDAEL_IV_', 'vdEUAZXRlTm75El5VtMWSQ==');
